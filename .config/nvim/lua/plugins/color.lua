0de73640c04b0f802d43dafa9d891027796e7e6f
Aino Spring <info@aino-spring.com>
2024-11-25 18:06:01 +0100

    Wallpaper, nvim and vesktop

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -14,7 +14,24 @@ return {
 			require("bamboo").setup({
 				transparent = true,
 			})
-			require('bamboo').load()
+			--require('bamboo').load()
 		end,
+	},
+
+	-- Use colors from pywal
+	{
+		"oncomouse/lushwal.nvim",
+		dependencies = {
+			{ "rktjmp/lush.nvim" },
+			{ "rktjmp/shipwright.nvim" },
+		},
+		config = function()
+			vim.g.lushwal_configuration = {
+				transparent_background = true,
+				terminal_colors = true
+			}
+			vim.cmd([[LushwalCompile]])
+			vim.cmd([[colorscheme lushwal]])
+		end
 	}
 }
